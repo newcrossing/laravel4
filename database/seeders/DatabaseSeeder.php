0454e960ae425becf786799aeb5a3f4f94d41dc6
newcrossing <newcrossing@gmail.com>
2021-01-25 20:55:34 +0300

    добавил шаблон главной

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(10)->create();
+     //    \App\Models\User::factory(10)->create();
+     //  \App\Models\Post::factory(10)->create();
+
+       // $users = User::factory()->count(3)->make();
+
+       // factory(App\Models\User::class)->create();
+
+      //  User::factory()->create();
+        Post::factory()->count(10)->create();
     }
 }
